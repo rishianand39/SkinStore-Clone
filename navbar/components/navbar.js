e05a2db10e609b2ee8714bf523658ae3a5a3ad8d
rishianand39 <rishi.rn818@gmail.com>
2022-02-25 22:43:18 +0530

    navbar done

---
+++ navbar/components/navbar.js
@@ -0,0 +1,147 @@
+function navbar() {
+    return `<div id="nabar-outer-div">
+    <div id="topbar">
+        <div>
+            <a href="#">
+                <img src="https://s3.thcdn.com/www/common/images/flags/1x1/us-853f40a4f4.svg" alt="" />
+                <span>us-USD</span>
+            </a>
+        </div>
+        <div>
+            <a href="#"> Help </a>
+        </div>
+    </div>
+
+    <!-- ----------Navbar------------- -->
+    <div>
+        <div id="navbar">
+            <div id="logo-div">
+                <a href="#">
+                    <img id="logo" src="https://s1.thcdn.com/enterprise/assets/skstr-global-113e023e-ecc9-45df-abad-eb92ce1956c9-logo-default.svg" alt="Navbar-logo" />
+                </a>
+            </div>
+            <div class="flex-div search-outer-div">
+                <div id="search-div">
+                    <input type="text" id="search-box" placeholder="Search for a product or brand..." />
+                    <img id="search-icon" src="/image/search.png" alt="" />
+                </div>
+            </div>
+            <div class="nav-right flex-div">
+                <a href="">
+                    <div class="ac-cart-div flex-div">
+                        <img src="/image/profile-icon.png" alt="" class="cart-icon" /> Account
+                    </div>
+                </a>
+                <a href="">
+                    <div class="ac-cart-div flex-div">
+                        <img src="/image/cart-icon.jpg" alt="" class="cart-icon" /> Cart
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="topnav" id="myTopnav">
+            <div class="dropdown">
+                <button class="dropbtn">Brand</button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Sale</button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Build a Routine</button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Skin Care</button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Hair</button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Makeup</button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Tools</button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Bath & Body</button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Self-Care</button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Fragrance</button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">New</button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Advice</button>
+                <div class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+            </div>
+
+            <a href="javascript:void(0);" style="font-size: 15px" class="icon" onclick="myFunction()">&#9776;</a
+      >
+    </div>
+  </div>
+</div>`;
+}
+export default navbar;
